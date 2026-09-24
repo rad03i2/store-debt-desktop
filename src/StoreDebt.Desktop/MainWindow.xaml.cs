4b16b2b17941df6f713a9145d009a5eb9bf7831b
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-24 17:33:29 +0300

    feat: wire phase two shortcuts dialogs and speech

--- src/StoreDebt.Desktop/MainWindow.xaml.cs
+++ src/StoreDebt.Desktop/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using System.Windows;
+using System.Windows.Input;
 using StoreDebt.Desktop.Data;
+using StoreDebt.Desktop.Services;
 using StoreDebt.Desktop.ViewModels;
 
 namespace StoreDebt.Desktop;
@@ -12,15 +14,45 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-        _viewModel = new MainViewModel(new StoreDatabase());
+        _viewModel = new MainViewModel(
+            new StoreDatabase(),
+            new DialogService(),
+            new DebtSpeechService());
+
         DataContext = _viewModel;
 
         Loaded += OnLoaded;
+        Closed += OnClosed;
     }
 
     private async void OnLoaded(object sender, RoutedEventArgs e)
     {
         Loaded -= OnLoaded;
         await _viewModel.InitializeAsync();
+        SearchBox.Focus();
+    }
+
+    private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.F)
+        {
+            if (_viewModel.IsActivityView)
+            {
+                ActivitySearchBox.Focus();
+                ActivitySearchBox.SelectAll();
+            }
+            else
+            {
+                SearchBox.Focus();
+                SearchBox.SelectAll();
+            }
+
+            e.Handled = true;
+        }
+    }
+
+    private void OnClosed(object? sender, EventArgs e)
+    {
+        _viewModel.Dispose();
     }
 }
